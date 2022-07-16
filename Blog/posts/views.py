8cf79ce50e386523c29016ee7810e373c0869b79
m-kowalewski <mickow95@gmail.com>
2022-07-16 13:24:16 +0200

    Initial commit
    Initial commit. Blog contains some basic functionality. There is possibility to create, modify and delete posts.

---
+++ Blog/posts/views.py
@@ -0,0 +1,33 @@
+""" views file for posts app """
+# from django.shortcuts import render
+from rest_framework import generics, permissions
+from . import serializers
+from .permissions import IsOwnerOrReadOnly
+from .models import Post
+from django.contrib.auth.models import User
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
---
+++ README.md
@@ -0,0 +1,2 @@
+# Blog App
+ Exercise. The purpose of this task is to prepare an E2E experience of a Blog.
