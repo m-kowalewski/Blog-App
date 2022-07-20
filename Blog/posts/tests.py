57fad1bbcb9d338effa1162aaca4dcb7f6f2eec7
m-kowalewski <mickow95@gmail.com>
2022-07-20 22:21:08 +0200

    some tests added
    Some basic tests added.

--- Blog/posts/tests.py
+++ Blog/posts/tests.py
@@ -1,3 +1,62 @@
+""" test file for posts app """
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from freezegun import freeze_time
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+
+class TestCreatePost(TestCase):
+
+    @freeze_time("2022-07-20 10:11:12")
+    def setUp(self):
+        test_user = User.objects.create_user(
+            username='test_user1',
+            password='1234567890'
+        )
+        test_user.save()
+        test_post = Post.objects.create(
+            title='Post title',
+            body='Post body',
+            owner=test_user
+        )
+        test_post.save()
+
+    def test_post_creation(self):
+        post = Post.objects.get(id=1)
+        created = f'{post.created}'
+        title = f'{post.title}'
+        body = f'{post.body}'
+        owner = f'{post.owner}'
+        self.assertEqual(created, '2022-07-20 10:11:12+00:00')
+        self.assertEqual(title, 'Post title')
+        self.assertEqual(body, 'Post body')
+        self.assertEqual(owner, 'test_user1')
+
+
+class TestAPI(APITestCase):
+
+    def setUp(self):
+        test_user = User.objects.create_user(
+            username='test_user1',
+            password='1234567890'
+        )
+        test_user.save()
+
+    def test_view_posts(self):
+        url = reverse('posts:posts_list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_creation_api(self):
+        data = {
+            'title': 'test_title_API',
+            'body': 'test_body_API',
+        }
+        self.client = APIClient()
+        self.client.login(username='test_user1', password='1234567890')
+        url = reverse('posts:posts_list')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- Blog/posts/urls.py
+++ Blog/posts/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
+app_name = 'posts'
 
 urlpatterns = [
-    path('users/', views.UserList.as_view()),
+    path('users/', views.UserList.as_view(), name='users_list'),
     path('users/<int:pk>/', views.UserDetail.as_view()),
-    path('posts/', views.PostList.as_view()),
+    path('posts/', views.PostList.as_view(), name='posts_list'),
     path('posts/<int:pk>/', views.PostDetail.as_view()),
 ]
 
